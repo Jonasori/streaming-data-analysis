bea079f3a9d6c85a228078676af0941377263819
Jonas Powell <jonasori@gmail.com>
2019-01-11 10:38:11 -0500

    Final commit

--- analysis.py
+++ analysis.py
@@ -38,11 +38,6 @@ def get_data():
 df = get_data()
 
 
-# Show some info.
-list(df.columns)
-list(df.browser.unique())
-list(df.isp.unique())
-
 
 # Make some subsets that will be useful for playing with.
 streams = [df[ df['stream_id'] == id ].reset_index()
@@ -229,7 +224,7 @@ plt.show()
 
 
 
-# ML Stuff:
+# Simple ML Stuff:
 
 def k_means(df, truncate=True, n=2):
 
@@ -256,60 +251,6 @@ def spectral_clustering(df, truncate=True, n=3):
     plt.scatter(X[:, 0], X[:, 1], c=labels,
                 s=50, cmap=cmap)
 
-    plt.show()
-
-def dbscan_testing():
-    """Drawn from the sklearn example."""
-    from sklearn.cluster import DBSCAN
-    from sklearn import metrics
-    from sklearn.datasets.samples_generator import make_blobs
-    from sklearn.preprocessing import StandardScaler
-
-    # My data
-    test_df = df.drop(df.index[5000:]) if truncate else df
-    X = np.array([np.array(test_df['cdn']), np.array(test_df['p2p'])]).T
-
-    # Generate sample data
-    centers = [[1, 1], [-1, -1], [1, -1]]
-    X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
-                                random_state=0)
-
-    X = StandardScaler().fit_transform(X)
-
-    clustering = DBSCAN(eps=3, min_samples=2).fit(X)
-    clustering.labels_
-    array([ 0,  0,  0,  1,  1, -1])
-    clustering
-
-    X.shape
-
-    db = DBSCAN().fit(X)
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
-    labels = db.labels_
-
-
-    # Plot result
-    # Black removed and is used for noise instead.
-    unique_labels = set(labels)
-    colors = [plt.cm.Spectral(each)
-              for each in np.linspace(0, 1, len(unique_labels))]
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            # Black used for noise.
-            col = [0, 0, 0, 1]
-
-        class_member_mask = (labels == k)
-
-        xy = X[class_member_mask & core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                 markeredgecolor='k', markersize=14)
-
-        xy = X[class_member_mask & ~core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                 markeredgecolor='k', markersize=6)
-
-    plt.title('Estimated number of clusters: %d' % n_clusters_)
     plt.show()
 
 
